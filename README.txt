e853a9139265313cc91ecb56eb1b1fa7c9f86dc8
Pierre Ficheux <pierre.ficheux@smile.fr>
2026-02-14 23:53:24 +0100

    Added WCH demo for L1-L4

--- README.txt
+++ README.txt
@@ -3,6 +3,11 @@ driver/
 ch368_io.c	I/O driver for BAR0
 led_blink.c	Led control for SDX, SCL
 
+wch_demo/
+
+l1_l4.c		L1-L4 control with WCH driver and library
+
 pics/
+
 SCL_GPIO.jpg	SCL GPIO connection
 
---
+++ wch_demo/l1_l4.c
@@ -0,0 +1,105 @@
+//
+// L1-L4 control using the official driver
+//
+#include <unistd.h>
+#include <errno.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include "ch36x_lib.h"
+
+static const char *device = "/dev/ch36xpci0";
+
+#define BYTE_TO_BINARY(byte)  \
+  ((byte) & 0x80 ? '1' : '0'), \
+  ((byte) & 0x40 ? '1' : '0'), \
+  ((byte) & 0x20 ? '1' : '0'), \
+  ((byte) & 0x10 ? '1' : '0'), \
+  ((byte) & 0x08 ? '1' : '0'), \
+  ((byte) & 0x04 ? '1' : '0'), \
+  ((byte) & 0x02 ? '1' : '0'), \
+  ((byte) & 0x01 ? '1' : '0')
+
+int toggleBit(int N, int K) {
+  
+    // Bitwise XOR with the mask
+    return (N ^ (1 << (K - 1)));
+}
+
+int main(int argc, char *argv[])
+{
+  int fd, i;
+  int ret;
+  char c;
+  enum CHIP_TYPE chiptype;
+  unsigned long iobase;
+  unsigned long membase;
+  int irq;
+  uint8_t mask;
+
+  fd = ch36x_open(device);
+  if (fd < 0) {
+    printf("ch36x_open error.\n");
+    goto exit;
+  }
+
+  ret = ch36x_get_chiptype(fd, &chiptype);
+  if (ret != 0) {
+    printf("ch36x_get_chiptype error.\n");
+    goto exit;
+  }
+  switch (chiptype) {
+  case CHIP_CH365:
+    printf("current chip model: CH365.\n");
+    break;
+  case CHIP_CH367:
+    printf("current chip model: CH367.\n");
+    break;
+  case CHIP_CH368:
+    printf("current chip model: CH368.\n");
+    break;
+  }
+
+  ret = ch36x_get_irq(fd, &irq);
+  if (ret != 0) {
+    printf("ch36x_get_irq error.\n");
+    goto exit;
+  }
+  printf("irq number:%d\n", irq);
+
+  ret = ch36x_get_ioaddr(fd, &iobase);
+  if (ret != 0) {
+    printf("ch36x_get_ioaddr error.\n");
+    goto exit;
+  }
+  printf("iobase:%lx\n", iobase);
+
+  if (chiptype == CHIP_CH368) {
+    ret = ch36x_get_memaddr(fd, &membase);
+    if (ret != 0) {
+      printf("ch36x_get_memaddr error.\n");
+      goto exit;
+    }
+    printf("membase:%lx\n", membase);
+  }
+
+  ret = ch36x_write_io_byte(fd, (uint8_t)0, 0x0f);
+
+  
+  mask = 0x01;
+  for (i = 0 ; i < 5 ; i++) {
+    printf ("i= %d  writing %02x  %c%c%c%c%c%c%c%c\n", i, ~mask, BYTE_TO_BINARY(mask));
+    ret = ch36x_write_io_byte(fd, (uint8_t)i, ~mask);
+    mask = mask << 1;
+    sleep (1);
+  }
+
+  ret = ch36x_close(fd);
+  if (ret != 0) 
+    printf("ch36x_close error.\n");
+ exit:
+  return ret;
+}
+
