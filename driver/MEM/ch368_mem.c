93edc93dd7834f477ef8b6fe026fbdc07d341cb7
Pierre Ficheux <pierre.ficheux@smile.fr>
2026-02-20 20:59:13 +0100

    Update MEM driver -> remove debug option

--- driver/MEM/ch368_mem.c
+++ driver/MEM/ch368_mem.c
@@ -1,3 +1,11 @@
+/*
+ * PCI demo driver (MEM based) for CH368 board
+ * 
+ * # insmod ch368_mem.ko
+ * # ./mem_test /dev/ch368_mem0
+ *
+ */
+
 #include <linux/kernel.h>	/* printk() */
 #include <linux/module.h>	/* modules */
 #include <linux/init.h>		/* module_{init,exit}() */
@@ -21,10 +29,6 @@ MODULE_LICENSE("GPL");
 static int major = 0; /* Major number */
 module_param(major, int, 0660);
 MODULE_PARM_DESC(major, "Static major number (none = dynamic)");
-
-static int debug = 0; 
-module_param(debug, int, 0660);
-MODULE_PARM_DESC(debug, "Debug flag (1 = YES, 0 = NO)");
 
 // Driver class in /sys
 static struct class *ch368_mem_class;
@@ -62,12 +66,11 @@ struct ch368_mem_struct {
 /*
  * Event handler
  */
-irqreturn_t ch368_mem_irq_handler(int irq, void *dev_id)
+static irqreturn_t ch368_mem_irq_handler(int irq, void *dev_id)
 {
   struct ch368_mem_struct *data = (struct ch368_mem_struct *)dev_id;
 
-  if (debug)
-    printk(KERN_INFO "ch368_mem: interrupt from device %d\n", data->minor);
+  printk(KERN_INFO "ch368_mem: interrupt from device %d\n", data->minor);
 
   return IRQ_HANDLED;
 }
@@ -88,15 +91,8 @@ static ssize_t ch368_mem_read(struct file *file, char *buf, size_t count, loff_t
   // Read data 
   for (i = 0 ; i < real ; i++) {
     *(p+i) = ioread8(data->mmio+ i + *ppos); 
-    if (debug)
-      pr_info ("%s: read %x @ %p (ppos= %lld)\n", __FUNCTION__, *(p+i), data->mmio+ i + *ppos, *ppos);
+    pr_info ("%s: read %x @ %p (ppos= %lld)\n", __FUNCTION__, *(p+i), data->mmio+ i + *ppos, *ppos);
   }
-
-/*
-  if (real)
-    if (copy_to_user((void __user *)buf, (void *)data->mmio + (int)*ppos, real))
-      return -EFAULT;
-*/
 
   *ppos += real;
 
@@ -107,25 +103,19 @@ static ssize_t ch368_mem_write(struct file *file, const char *buf, size_t count,
 {
   int i, real;
   struct ch368_mem_struct *data = file->private_data;
-  unsigned char *p = (unsigned char *)buf;
+  unsigned char *p = (unsigned char *)buf, c;
   
   /* Check for overflow */
   real = min(data->mmio_len - (u64)*ppos, (u64) count);
 
   // Write data
   for (i = 0 ; i < real ; i++) {
-    if (debug)
-      pr_info ("%s: write %x @ ppos= %d\n", __FUNCTION__, *(p+i), (int)*ppos);
+    pr_info ("%s: write %x @ ppos= %d\n", __FUNCTION__, *(p+i), (int)*ppos);
     iowrite8(*(p+i), data->mmio + *ppos + i);
-    if (debug) {
-      unsigned char c;
-      c = ioread8(data->mmio + i + *ppos); 
-      pr_info ("%s: written %x @ ppos %d\n", __FUNCTION__, c, (int)*ppos);
-    }
 
+    c = ioread8(data->mmio + i + *ppos); 
+    pr_info ("%s: written %x @ ppos %d\n", __FUNCTION__, c, (int)*ppos);
   }
-
-
   
   *ppos += real;
 
@@ -138,8 +128,7 @@ static int ch368_mem_open(struct inode *inode, struct file *file)
   struct list_head *cur;
   struct ch368_mem_struct *data;
 
-  if (debug)
-    printk("ch368_mem_open()\n");
+  printk("ch368_mem_open()\n");
 
   list_for_each(cur, &ch368_mem_list) {
     data = list_entry(cur, struct ch368_mem_struct, link);
@@ -151,15 +140,14 @@ static int ch368_mem_open(struct inode *inode, struct file *file)
     }
   }
 
-  printk(KERN_WARNING "ch368_mem: minor %d not found\n", minor);
+  pr_warn("ch368_mem: minor %d not found\n", minor);
 
   return -ENODEV;
 }
 
 static int ch368_mem_release(struct inode *inode, struct file *file)
 {
-  if (debug)
-    printk("ch368_mem_release()\n");
+  printk("ch368_mem_release()\n");
 
   file->private_data = NULL;
 
@@ -184,13 +172,13 @@ static int ch368_mem_probe(struct pci_dev *dev, const struct pci_device_id *ent)
   static int minor = 0;
   struct device *device = NULL;
 
-  printk(KERN_INFO "ch368_mem: found %04x:%04x\n", ent->vendor, ent->device);
-  printk(KERN_INFO "ch368_mem: using major %d and minor %d for this device\n", major, minor);
+  pr_info("ch368_mem: found %04x:%04x\n", ent->vendor, ent->device);
+  pr_info("ch368_mem: using major %d and minor %d for this device\n", major, minor);
 
   /* Allocate a private structure and reference it as driver's data */
   data = (struct ch368_mem_struct *)kmalloc(sizeof(struct ch368_mem_struct), GFP_KERNEL);
   if (data == NULL) {
-    printk(KERN_WARNING "ch368_mem: unable to allocate private structure\n");
+    pr_warn("ch368_mem: unable to allocate private structure\n");
 
     ret = -ENOMEM;
     goto cleanup_kmalloc;
@@ -205,7 +193,7 @@ static int ch368_mem_probe(struct pci_dev *dev, const struct pci_device_id *ent)
   /* Initialize device before it's used by the driver */
   ret = pci_enable_device(dev);
   if (ret < 0) {
-    printk(KERN_WARNING "ch368_mem: unable to initialize PCI device\n");
+    pr_warn("ch368_mem: unable to initialize PCI device\n");
 
     goto cleanup_pci_enable;
   }
@@ -213,7 +201,7 @@ static int ch368_mem_probe(struct pci_dev *dev, const struct pci_device_id *ent)
   /* Reserve PCI I/O and memory resources */
   ret = pci_request_regions(dev, "ch368_mem");
   if (ret < 0) {
-    printk(KERN_WARNING "ch368_mem: unable to reserve PCI resources\n");
+    pr_warn("ch368_mem: unable to reserve PCI resources\n");
 
     goto cleanup_regions;
   }
@@ -230,13 +218,13 @@ static int ch368_mem_probe(struct pci_dev *dev, const struct pci_device_id *ent)
     if (pci_resource_flags(dev, i) & IORESOURCE_MEM) {
       data->mmio = pci_iomap(dev, i, pci_resource_len(dev, i));
       if (data->mmio == NULL) {
-	printk(KERN_WARNING "ch368_mem: unable to remap BAR %d\n", i);
+	pr_warn("ch368_mem: unable to remap BAR %d\n", i);
 	goto cleanup_pci_iomap;
       }
 
       data->mmio_len = pci_resource_len(dev, i);
 
-      printk(KERN_INFO "ch368_mem: BAR %d has been remaped at 0x%p\n", i, data->mmio);
+      pr_info("ch368_mem: BAR %d has been remaped at 0x%p\n", i, data->mmio);
 
       break; // we use the first MEM
     }
@@ -252,18 +240,18 @@ static int ch368_mem_probe(struct pci_dev *dev, const struct pci_device_id *ent)
 #ifdef USE_IRQ
   if (dev->pin) {
     if (pci_enable_msi (dev))
-      printk(KERN_WARNING "ch368_mem: unable to init MSI !\n");
+      pr_warn("ch368_mem: unable to init MSI !\n");
     else 
       ret = request_irq(dev->irq, ch368_mem_irq_handler, 0, "ch368_mem", data);
     if (ret < 0) {
-      printk(KERN_WARNING "ch368_mem: unable to register irq handler\n");
+      pr_warn("ch368_mem: unable to register irq handler\n");
       goto cleanup_irq;
     }
     else
-      printk(KERN_WARNING "ch368_mem: IRQ %d registered !\n", dev->irq);
+      pr_warn("ch368_mem: IRQ %d registered !\n", dev->irq);
   }
   else
-    printk(KERN_INFO "ch368_mem: no IRQ!\n");
+    pr_info("ch368_mem: no IRQ!\n");
 #endif
 
   /* Link the new data structure with others */
@@ -274,7 +262,7 @@ static int ch368_mem_probe(struct pci_dev *dev, const struct pci_device_id *ent)
 
   if (IS_ERR(device)) {
     ret = PTR_ERR(device);
-    printk(KERN_WARNING "ch368_mem: can't create device %d\n", data->minor);
+    pr_warn("ch368_mem: can't create device %d\n", data->minor);
     goto cleanup_irq;
   }
 
@@ -315,7 +303,7 @@ static void ch368_mem_remove(struct pci_dev *dev)
 
   kfree(data);
 
-  printk(KERN_INFO "ch368_mem: device removed\n");
+  pr_info("ch368_mem: device removed\n");
 }
 
 static struct pci_driver ch368_mem_driver = {
@@ -335,7 +323,7 @@ static int __init ch368_mem_init(void)
   /* Register the device driver major */
   ret = register_chrdev(major, "ch368_mem", &ch368_mem_fops);
   if (ret < 0) {
-    printk(KERN_WARNING "ch368_mem: unable to get a major\n");
+    pr_warn("ch368_mem: unable to get a major\n");
 
     return ret;
   }
@@ -344,10 +332,14 @@ static int __init ch368_mem_init(void)
     major = ret; /* dynamic value */
 
   // Class creation in /sys/class
+  #if LINUX_VERSION_CODE <= KERNEL_VERSION(6, 4, 0)
   ch368_mem_class = class_create (THIS_MODULE, "ch368_mem");
+#else
+  ch368_mem_class = class_create ("ch368_mem");
+#endif  
   if (IS_ERR(ch368_mem_class)) {
     ret = PTR_ERR(ch368_mem_class);
-    printk(KERN_WARNING "ch368_mem: can't create class !\n");
+    pr_warn("ch368_mem: can't create class !\n");
 
     return ret;
   }
@@ -355,7 +347,7 @@ static int __init ch368_mem_init(void)
   /* Register PCI driver */
   ret = pci_register_driver(&ch368_mem_driver);
   if (ret < 0) {
-    printk(KERN_WARNING "ch368_mem: unable to register PCI driver\n");
+    pr_warn("ch368_mem: unable to register PCI driver\n");
 
     unregister_chrdev(major, "ch368_mem");
 
